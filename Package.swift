2d602a24777c8a994c319763169d66ee5a091eff
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-08 02:21:53 -0500

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
 	name: "NewDocs",
 	platforms: [
-		.macOS(.v13),
+		.macOS(.v15),
 		.iOS(.v16),
 	],
 	products: [
