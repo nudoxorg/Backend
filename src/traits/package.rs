ccbb4617d49330131595269b10fcf96c90af0f60
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-18 07:04:10 -0500

    Added a package trait

---
+++ src/traits/package.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+use semver::Version;
+use url::Url;
+
+pub trait Package {
+    // The name you'd expect to see it referred to as (Can just be a derivative of the slug)
+    fn name(&self) -> &str;
+
+    // The battle-ready slug for encoding and references
+    fn slug(&self) -> &str;
+
+    // The precise version of the doc
+    fn version(&self) -> &Version;
+
+    // Any extraneous links like the source page, or the projects home
+    fn links(&self) -> &HashMap<String, Url>;
+
+    // Builds the pages and sends them off to mongo?
+    async fn build_pages(&self) -> Result<(), anyhow::Error>;
+}
