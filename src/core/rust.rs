1de99c1141c88ab8caa434bd016e55ff03e21e9a
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-18 07:20:57 -0500

    General rust scaffolding

---
+++ src/core/rust.rs
@@ -0,0 +1,39 @@
+use crates_io_api::{CratesQuery, SyncClient};
+
+use crate::traits::registry::Registry;
+
+pub struct Crates {
+    client: SyncClient,
+}
+
+impl Registry for Crates {
+    async fn search_packages(
+        &self,
+        query: &str,
+    ) -> Result<Vec<Box<dyn crate::traits::package::Package>>, PackageRegistryError> {
+        let query = CratesQuery::builder().search(query).build();
+        let result = self.client.crates(query)?;
+
+        result.crates
+
+        // Frankly do not care about multi-page results right now
+    }
+
+    async fn get_package_by_uuid(
+        &self,
+        uuid: u64,
+    ) -> Result<Box<dyn crate::traits::package::Package>, PackageRegistryError> {
+        todo!()
+    }
+
+    async fn get_packages_by_name(
+        &self,
+        name: &str,
+    ) -> Result<Vec<Box<dyn crate::traits::package::Package>>, PackageRegistryError> {
+        todo!()
+    }
+
+    async fn get_reference(&self) -> Box<dyn crate::traits::package::Package> {
+        todo!()
+    }
+}
