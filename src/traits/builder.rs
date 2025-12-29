8697f957c9da7020640cc5b6e25c119f0deeee18
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-28 21:44:01 -0600

    Completed signatures

--- src/traits/builder.rs
+++ src/traits/builder.rs
@@ -1,11 +1,19 @@
+use std::time::Duration;
+
+use crates_io_api::SyncClient;
 use lang_types::Language;
 
-use crate::traits::registry::Registry;
+use crate::{
+    core::rust::{Crates, RPackage},
+    traits::{package::Package, registry::Registry},
+};
 
 pub fn get_registry(language: Language) -> impl Registry {
     // There's a large set of languages we're yet to support unfortunately
     match language {
-        Language::Rust => todo!(),
+        Language::Rust => Crates {
+            client: SyncClient::new("my_bot (help@my_bot.com)", Duration::from_secs(1)).unwrap(),
+        },
         other => todo!(),
     }
 }
