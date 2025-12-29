f026af99d28542cd5fe2cd9bc20d2e06ec4764fa
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-28 21:50:29 -0600

    Expanded main function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use crate::traits::{
+    builder::{self, get_registry},
+    registry::Registry,
+};
+
 mod core;
+mod error;
 mod ir;
 mod traits;
-mod error;
 
 fn main() {
     println!("Hello, world!");
+
+    let registry = get_registry(lang_types::Language::Rust);
+
+    dbg!(registry.get_reference());
 }
