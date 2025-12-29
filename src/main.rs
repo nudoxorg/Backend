3c7053b0b0e322ebab912b56ae3c21982a021018
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-29 15:27:57 -0600

    Cleaned up main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use semver::Version;
+
 use crate::traits::{
     builder::{self, get_registry},
     registry::Registry,
@@ -8,8 +10,21 @@ mod error;
 mod ir;
 mod traits;
 
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
 
+#[tokio::main]
+async fn main() {
     let registry = get_registry(lang_types::Language::Rust);
+    let packages = registry.get_packages_by_name("axum").await;
+    let out = match packages {
+        Ok(mut packages) => {
+            let pkg = packages.remove(0); // Take ownership by removing from vec
+            tokio::task::spawn_blocking(move || pkg.retrieve(Version::new(0, 8, 8), None))
+                .await
+                .unwrap()
+        }
+        Err(_other) => todo!(),
+    }
+    .unwrap();
+    dbg!(out);
 }
