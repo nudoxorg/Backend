df780e0aff0833751094a53835b4630287bd1ad3
Myles Wirth <regime_skylark.0y@icloud.com>
2026-01-07 15:51:26 -0500

    Fixed IR to actually hold members

--- src/ir/entry.rs
+++ src/ir/entry.rs
@@ -20,7 +20,7 @@ pub struct Entry {
     pub documentation: Option<String>, // The associated documentation
 
     // Added missing properties
-    pub members: Option<Vec<String>>,
+    pub members: Option<Vec<Entry>>,
     pub input_parameters: Option<Vec<Parameter>>,
     pub output_parameters: Option<Vec<Parameter>>,
     pub type_parameters: Option<Vec<String>>,
