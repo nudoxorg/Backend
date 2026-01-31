bbabdaa64f7436ba72315e26ce10ed92ef584a62
Myles Wirth <regime_skylark.0y@icloud.com>
2026-01-30 23:30:32 -0500

    initial npins integration

--- default.nix
+++ default.nix
@@ -1,11 +1,14 @@
 {
+  # Import packages
+  sources ? (import ./npins),
+  nixpkgs ? sources.nixpkgs,
+
   system ? builtins.currentSystem,
+  pkgs ? (import nixpkgs { inherit system; }),
+
 
   # Fenix for nightly
   fenix ? import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") { },
-
-  # TODO: Switch to npins for reproducibility (Also port fenix)
-  pkgs ? import <nixpkgs> { inherit system; },
 }:
 let
   rust-nightly = fenix.complete.withComponents [
