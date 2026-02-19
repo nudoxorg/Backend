e310ecc40ca437f76f4dbfce6224b021882e28dd
Myles Wirth <regime_skylark.0y@icloud.com>
2026-02-19 07:58:33 -0500

    formatting

--- ir/kind.rs
+++ ir/kind.rs
@@ -7,16 +7,16 @@ use serde::{Deserialize, Serialize};
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "facet", derive(Facet), repr(C))]
 pub enum Visibility {
-    #[cfg_attr(feature = "serde", serde(rename = "public"))]
-    Public,
-    #[cfg_attr(feature = "serde", serde(rename = "private"))]
-    Private,
-    #[cfg_attr(feature = "serde", serde(rename = "protected"))]
-    Protected,
-    #[cfg_attr(feature = "serde", serde(rename = "internal"))]
-    Internal,
-    #[cfg_attr(feature = "serde", serde(rename = "package"))]
-    Package,
+	#[cfg_attr(feature = "serde", serde(rename = "public"))]
+	Public,
+	#[cfg_attr(feature = "serde", serde(rename = "private"))]
+	Private,
+	#[cfg_attr(feature = "serde", serde(rename = "protected"))]
+	Protected,
+	#[cfg_attr(feature = "serde", serde(rename = "internal"))]
+	Internal,
+	#[cfg_attr(feature = "serde", serde(rename = "package"))]
+	Package,
 }
 
 /// Represents different kinds of code elements in a programming language or
@@ -26,126 +26,121 @@ pub enum Visibility {
 #[cfg_attr(feature = "serde", serde(tag = "kind", content = "value"))]
 #[cfg_attr(feature = "facet", derive(Facet), repr(C))]
 pub enum Kind {
-    /// A namespace, package, or module.
-    Module,
+	/// A namespace, package, or module.
+	Module,
 
-    /// Struct, class, record, or data class.
-    #[cfg_attr(feature = "serde", serde(rename = "recordType"))]
-    RecordType(Record),
+	/// Struct, class, record, or data class.
+	#[cfg_attr(feature = "serde", serde(rename = "recordType"))]
+	RecordType(Record),
 
-    /// Unlinked documentation
-    Info,
+	/// Unlinked documentation
+	Info,
 
-    /// Union type
-    #[cfg_attr(feature = "serde", serde(rename = "unionType"))]
-    UnionType(Vec<Type>),
+	/// Union type
+	#[cfg_attr(feature = "serde", serde(rename = "unionType"))]
+	UnionType(Vec<Type>),
 
-    /// Trait/protocol/interface definition
-    #[cfg_attr(feature = "serde", serde(rename = "traitDef"))]
-    TraitDef(TraitDef),
+	/// Trait/protocol/interface definition
+	#[cfg_attr(feature = "serde", serde(rename = "traitDef"))]
+	TraitDef(TraitDef),
 
-    /// Trait/protocol implementation
-    #[cfg_attr(feature = "serde", serde(rename = "traitImpl"))]
-    TraitImpl(TraitImpl),
+	/// Trait/protocol implementation
+	#[cfg_attr(feature = "serde", serde(rename = "traitImpl"))]
+	TraitImpl(TraitImpl),
 
-    /// Enum, algebraic data type, discriminated union.
-    #[cfg_attr(feature = "serde", serde(rename = "sumType"))]
-    SumType(Vec<SumVariant>),
+	/// Enum, algebraic data type, discriminated union.
+	#[cfg_attr(feature = "serde", serde(rename = "sumType"))]
+	SumType(Vec<SumVariant>),
 
-    /// Trait, interface, abstract base class.
-    #[cfg_attr(feature = "serde", serde(rename = "interfaceType"))]
-    InterfaceType,
+	/// Trait, interface, abstract base class.
+	#[cfg_attr(feature = "serde", serde(rename = "interfaceType"))]
+	InterfaceType,
 
-    /// Function, method, lambda (with metadata).
-    Function(Function),
+	/// Function, method, lambda (with metadata).
+	Function(Function),
 
-    /// Type alias, typedef, using alias.
-    #[cfg_attr(feature = "serde", serde(rename = "typeAlias"))]
-    TypeAlias(Type),
+	/// Type alias, typedef, using alias.
+	#[cfg_attr(feature = "serde", serde(rename = "typeAlias"))]
+	TypeAlias(Type),
 
-    // value in compler output for now... Quick PAtch for now
-    /// Constant or immutable global.
-    Constant,
+	// value in compler output for now... Quick PAtch for now
+	/// Constant or immutable global.
+	Constant,
 
-    /// Mutable global/static variable.
-    Variable,
+	/// Mutable global/static variable.
+	Variable,
 
-    /// Macro, template, codegen hook.
-    Macro,
+	/// Macro, template, codegen hook.
+	Macro,
 
-    /// Built‑in primitive type.
-    #[cfg_attr(feature = "serde", serde(rename = "primitiveType"))]
-    PrimitiveType,
+	/// Built‑in primitive type.
+	#[cfg_attr(feature = "serde", serde(rename = "primitiveType"))]
+	PrimitiveType,
 
-    /// Field or property of a type.
-    Field,
+	/// Field or property of a type.
+	Field,
 
-    /// Event, signal, or callback definition.
-    Event,
+	/// Event, signal, or callback definition.
+	Event,
 }
 
 // Implementing `Display` for `Kind` to replace the Swift `description` computed
 // property.
 use std::fmt;
 
-use crate::{
-    function::Function,
-    protocols::{TraitDef, TraitImpl},
-    record::{Record, SumVariant},
-    ty::Type,
-};
+use crate::{function::Function, protocols::{TraitDef, TraitImpl}, record::{Record, SumVariant}, ty::Type};
 
 impl fmt::Display for Kind {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let description = match self {
-            // A namespace, package, or module.
-            Kind::Module => "Module",
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		let description = match self {
+			// A namespace, package, or module.
+			Kind::Module => "Module",
 
-            // Struct, class, record, or data class.
-            Kind::RecordType(_) => "RecordType",
+			// Struct, class, record, or data class.
+			Kind::RecordType(_) => "RecordType",
 
-            // A piece of unlinked documentation
-            Kind::Info => "Info",
+			// A piece of unlinked documentation
+			Kind::Info => "Info",
 
-            // Union type (C, C++, Rust, etc.).
-            Kind::UnionType(_) => "UnionType",
+			// Union type (C, C++, Rust, etc.).
+			Kind::UnionType(_) => "UnionType",
 
-            // Trait def
-            Kind::TraitDef(_) => "TraitDef",
+			// Trait def
+			Kind::TraitDef(_) => "TraitDef",
 
-            // Trait impl
-            Kind::TraitImpl(_) => "TraitImpl",
+			// Trait impl
+			Kind::TraitImpl(_) => "TraitImpl",
 
-            // Enum, algebraic data type, discriminated union.
-            Kind::SumType(_) => "SumType",
+			// Enum, algebraic data type, discriminated union.
+			Kind::SumType(_) => "SumType",
 
-            // Trait, interface, abstract base class.
-            Kind::InterfaceType => "InterfaceType",
+			// Trait, interface, abstract base class.
+			Kind::InterfaceType => "InterfaceType",
 
-            // Function, method, lambda (with metadata).
-            Kind::Function(_) => "Function",
+			// Function, method, lambda (with metadata).
+			Kind::Function(_) => "Function",
 
-            // Type alias, typedef, using alias.
-            Kind::TypeAlias(_) => "TypeAlias",
+			// Type alias, typedef, using alias.
+			Kind::TypeAlias(_) => "TypeAlias",
 
-            // Constant or immutable global.
-            Kind::Constant => "Constant",
+			// Constant or immutable global.
+			Kind::Constant => "Constant",
 
-            // Mutable global/static variable.
-            Kind::Variable => "Variable",
+			// Mutable global/static variable.
+			Kind::Variable => "Variable",
 
-            // Macro, template, codegen hook.
-            Kind::Macro => "Macro",
+			// Macro, template, codegen hook.
+			Kind::Macro => "Macro",
 
-            // Built-in primitive type.
-            Kind::PrimitiveType => "PrimitiveType",
+			// Built-in primitive type.
+			Kind::PrimitiveType => "PrimitiveType",
 
-            // Field or property of a type.
-            Kind::Field => "Field",
+			// Field or property of a type.
+			Kind::Field => "Field",
 
-            // Event, signal, or callback definition.
-            Kind::Event => "Event",
-        };
-        write!(f, "{}", description)
-    }
+			// Event, signal, or callback definition.
+			Kind::Event => "Event",
+		};
+		write!(f, "{}", description)
+	}
 }
--- ir/record.rs
+++ ir/record.rs
@@ -3,31 +3,27 @@ use facet::Facet;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    generics::{ConstExpr, GenericArg},
-    kind::Visibility,
-    ty::Type,
-};
+use crate::{generics::{ConstExpr, GenericArg}, kind::Visibility, ty::Type};
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "facet", derive(Facet))]
 pub struct Record {
-    /// Optional name of the record (e.g., "User", "Point").
-    /// Anonymous records (like tuples or JS objects) may omit this.
-    pub name: Option<String>,
+	/// Optional name of the record (e.g., "User", "Point").
+	/// Anonymous records (like tuples or JS objects) may omit this.
+	pub name: Option<String>,
 
-    /// Optional generic parameters (e.g., <T, U>).
-    pub generics: Option<Vec<GenericArg>>,
+	/// Optional generic parameters (e.g., <T, U>).
+	pub generics: Option<Vec<GenericArg>>,
 
-    /// The kind of record (named, tuple, unit, dynamic).
-    pub kind: RecordKind,
+	/// The kind of record (named, tuple, unit, dynamic).
+	pub kind: RecordKind,
 
-    /// The fields of the record (if applicable).
-    pub fields: Option<Vec<Field>>,
+	/// The fields of the record (if applicable).
+	pub fields: Option<Vec<Field>>,
 
-    /// The visibility of the record
-    pub visibility: Option<Visibility>,
+	/// The visibility of the record
+	pub visibility: Option<Visibility>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -35,43 +31,43 @@ pub struct Record {
 #[cfg_attr(feature = "facet", derive(Facet), repr(C))]
 #[serde(rename_all_fields = "snake_case")]
 pub enum RecordKind {
-    /// A record with no fields (unit struct, empty object).
-    Unit,
-    /// A record with ordered, positional fields (tuple, tuple struct).
-    Tuple,
-    /// A record with named fields (struct, class, record, object).
-    Named,
-    /// A record with dynamic/unknown fields (JS object, Python dict).
-    Dynamic,
+	/// A record with no fields (unit struct, empty object).
+	Unit,
+	/// A record with ordered, positional fields (tuple, tuple struct).
+	Tuple,
+	/// A record with named fields (struct, class, record, object).
+	Named,
+	/// A record with dynamic/unknown fields (JS object, Python dict).
+	Dynamic,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "facet", derive(Facet))]
 pub struct Field {
-    /// Field name (None if tuple-like).
-    pub name: Option<String>,
+	/// Field name (None if tuple-like).
+	pub name: Option<String>,
 
-    pub type_entry_id: Option<i64>,
+	pub type_entry_id: Option<i64>,
 
-    /// Type of the field (if known).
-    pub ty: Option<Box<Type>>,
-    /// The default value
-    pub default_value: Option<ConstExpr>,
+	/// Type of the field (if known).
+	pub ty:            Option<Box<Type>>,
+	/// The default value
+	pub default_value: Option<ConstExpr>,
 
-    /// Attributes on the field
-    pub attributes: Option<FieldAttribute>,
+	/// Attributes on the field
+	pub attributes: Option<FieldAttribute>,
 
-    /// Visibility
-    pub visibility: Option<Visibility>,
+	/// Visibility
+	pub visibility: Option<Visibility>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "facet", derive(Facet), repr(C))]
 pub enum FieldAttribute {
-    Mutable,
-    Optional,
+	Mutable,
+	Optional,
 }
 
 // Adding sum variants here for historical reasons
@@ -80,9 +76,9 @@ pub enum FieldAttribute {
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg_attr(feature = "facet", derive(Facet))]
 pub struct SumVariant {
-    /// The variant/tag name (e.g., "Some", "None", "Ok", "Err")
-    pub name: String,
-    /// Associated types for this variant (None for unit variants)
-    pub types: Option<Vec<Type>>, /* has to be vec because it could technically hold more than
-                                   * one?? Might want to use an enum for this idk */
+	/// The variant/tag name (e.g., "Some", "None", "Ok", "Err")
+	pub name:  String,
+	/// Associated types for this variant (None for unit variants)
+	pub types: Option<Vec<Type>>, /* has to be vec because it could technically hold more than
+	                               * one?? Might want to use an enum for this idk */
 }
