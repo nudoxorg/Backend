398ef7362e408925a030d63430b3f3250a9a87d5
Myles Wirth <regime_skylark.0y@icloud.com>
2026-02-21 10:29:25 -0500

    fix: entry definitions

--- flake.nix
+++ flake.nix
@@ -72,11 +72,13 @@
               convco = prePushHook {
                 enable = true;
                 pass_filenames = false;
-                entry = pkgs.writeShellScript "convco-pre-push" ''
-                  while read local_ref local_sha remote_ref remote_sha; do
-                    ${pkgs.convco}/bin/convco check "$remote_sha..$local_sha"
-                  done
-                '';
+                entry = toString (
+                  pkgs.writeShellScript "convco-pre-push" ''
+                    while read local_ref local_sha remote_ref remote_sha; do
+                      ${pkgs.convco}/bin/convco check "$remote_sha..$local_sha"
+                    done
+                  ''
+                );
               };
               rustfmt = prePushHook {
                 enable = true;
