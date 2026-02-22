00ef5e3914178e4cf79a9db8a050d919d4b8c801
Myles Wirth <regime_skylark.0y@icloud.com>
2026-02-21 19:36:23 -0500

    chore: documentation of the flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,17 @@
   description = "NuNuShell development environment";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,6 +27,7 @@
       devshell,
     }:
     let
+      # Everything that Nix supports right now
       systems = [
         "aarch64-darwin"
         "aarch64-linux"
@@ -51,6 +55,7 @@
           ...
         }:
         let
+          # Nightly enables a lot of nice things, but mainly it allows us to build with rustfmt
           rust-nightly = fenix-pkg.complete.withComponents [
             "cargo"
             "clippy"
@@ -62,11 +67,13 @@
         {
           pre-commit-check = git-hooks.lib.${system}.run {
             src = ./.;
-            package = pkgs.prek;
+            package = pkgs.prek; # Prek for parellelizism
             default_stages = [ "pre-push" ];
             hooks = {
-              nixfmt.enable = true;
-
+              # We have convco here to enforce commit messages
+              # The goal here is to NOT slow down dev productivity
+              # So we're encouraging a workflow where, if you're in "flow"
+              # Just don't worry about pushing. The annoying part can hit then
               convco = {
                 enable = true;
                 pass_filenames = false;
@@ -77,7 +84,12 @@
                     done
                   ''
                 );
+                stages = [ "pre-push" ];
               };
+
+              # Formatting is PURELY for QOL
+              # Consistency is key for building patterns, and to that end, a priority should be enabling reliable dev setups so this doesn't trip up on pre-push
+              nixfmt.enable = true;
 
               rustfmt = {
                 enable = true;
@@ -93,6 +105,8 @@
                 language = "system";
               };
 
+              # The main branch needs to always be green
+              # Both passing all tests and avoiding any clippy lints
               testrust = {
                 enable = true;
                 name = "testrust";
@@ -106,6 +120,10 @@
                 enable = true;
                 packageOverrides.cargo = rust-nightly;
                 packageOverrides.clippy = rust-nightly;
+                stages = [
+                  "pre-merge-commit"
+                  "pre-push"
+                ];
               };
             };
           };
@@ -128,7 +146,9 @@
             "rustfmt"
             "rustc-codegen-cranelift-preview"
           ];
-          pre-commit-check = self.checks.${system}.pre-commit-check;
+          hooks = self.checks.${system}.pre-commit-check;
+
+          # Sourcing from nushell for our commands
           mkCommand = name: help: category: {
             inherit name help category;
             command = "cd $PRJ_ROOT && nu .config/scripts/${name}.nu \"$@\"";
@@ -139,6 +159,7 @@
             name = "NuNuShell";
             env = [
               {
+                # TODO: See if there's a more reliable way to avoid this, just a linker issue I started facing
                 name = "LIBRARY_PATH";
                 value = "$(nix eval --raw nixpkgs#libiconv.outPath)/lib";
               }
@@ -157,32 +178,29 @@
             '';
 
             packages = [
-              rust-nightly
-              pkgs.nushell
-              pkgs.ollama
-              pkgs.git
-              pkgs.clang
-              pkgs.cargo-bump # Version bumping
-              pkgs.jujutsu
-              pkgs.rust-analyzer
-              pkgs.flock
-              pkgs.nixfmt
-              pkgs.tombi
-              pkgs.typos
-              pkgs.hongdown
-              pkgs.just
-              pkgs.radicle-node
-              pkgs.radicle-tui
-              pkgs.headscale
-              pkgs.kittysay
-              pkgs.marksman
-              pkgs.taplo
-              pkgs.cargo-nextest
-              pkgs.libiconv
-              pkgs.nil
-              pkgs.dotacat # Rust lolcat
+              rust-nightly # Rust nightly toolchain
+              pkgs.git # Version control
+              pkgs.cargo-bump # Bump crate versions
+              pkgs.rust-analyzer # Rust LSP server
+              pkgs.flock # For managing shell concurrency
+              pkgs.nixfmt # Nix formatter
+              pkgs.tombi # TOML formatter/linter
+              pkgs.typos # Source code spell checker
+              pkgs.hongdown # Markdown formatting
+              pkgs.radicle-node # P2P code collaboration
+              pkgs.radicle-tui # Radicle terminal UI
+              pkgs.kittysay # Cat ASCII art
+              pkgs.marksman # Markdown LSP server
+              pkgs.taplo # TOML LSP/formatter
+              pkgs.cargo-nextest # Next-gen test runner
+              pkgs.libiconv # Character encoding library, associated with linker error
+              pkgs.nil # Nix LSP server
+              pkgs.dotacat # Colorful terminal output
             ]
-            ++ pkgs.lib.optional pkgs.stdenv.isLinux pkgs.wild;
+            ++ pkgs.lib.optional pkgs.stdenv.isLinux [
+              pkgs.wild
+              pkgs.clang
+            ]; # Fast linker (RUST), only works with clang for now
 
             commands = [
               # --- Build & Check --- #
@@ -229,7 +247,7 @@
             ];
             devshell.startup.shellHook.text = ''
               export RUST_TARGET=$(rustc --version --verbose | grep '^host:' | awk '{print $2}')
-              ${pre-commit-check.shellHook}
+              ${hooks.shellHook}
               (
                 # Use a lockfile to prevent multiple instances from stomping on Git
                 flock -n 9 || exit 1
@@ -242,6 +260,7 @@
           };
         }
       );
+
       # Expose devShell as a package for `nix shell` compatibility
       packages = eachSystem (
         { system, ... }:
