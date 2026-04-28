fec42e36893e6709e22fb8465cd2e3a6bc7f2c92
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-28 17:35:45 -0400

    Huge work on redefining primitives

--- ir/ty.rs
+++ ir/ty.rs
@@ -51,7 +51,66 @@ pub struct DynTrait {
 	pub lifetime: Option<String>,
 }
 
-// MARK: - FunctionPointer
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct GenericParam {
+	pub name: String,
+
+	/// For Higher-Kinded Types (HKTs), define the expected "shape" of the generic.
+	pub kind: Option<Box<Type>>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum Primitive {
+	/// Signed Integers (8, 16, 32, 64, 128, and Architecture-dependent)
+	Int(IntWidth),
+
+	/// Unsigned Integers (8, 16, 32, 64, 128, and Architecture-dependent)
+	UInt(IntWidth),
+
+	/// Floating point numbers
+	Float(FloatWidth),
+
+	/// Boolean logic
+	Bool,
+
+	/// UTF-8 or similar string types
+	String,
+
+	/// Single character type
+	Char,
+
+	/// Binary data / Byte buffers
+	Bytes,
+
+	/// Temporal/Date types
+	Date,
+
+	/// A pointer-sized address (void*)
+	Address,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum IntWidth {
+	W8,
+	W16,
+	W32,
+	W64,
+	W128,
+
+	/// Machine dependent/usize
+	Arch,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum FloatWidth {
+	W16,
+	W32,
+	W64,
+}
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
