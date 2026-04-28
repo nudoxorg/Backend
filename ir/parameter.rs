81ffde03026ab7e555ce134b46cef0dd3457f907
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-28 17:43:55 -0400

    Expansions to parameter

--- ir/parameter.rs
+++ ir/parameter.rs
@@ -1,17 +1,25 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-use crate::{generics::ConstExpr, ty::Type};
+use crate::{
+	generics::{ConstExpr, TypeParam},
+	ty::Type,
+};
 
 /// Represents a parameter in a function or method.
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct Parameter {
-	pub name:          String,
-	pub ty:            Option<Type>, // Renamed 'type' to 'ty'
-	pub attributes:    Option<Vec<ParameterAttribute>>,
+pub struct LiteralParameter {
+	pub name: String,
+	pub r#type: Option<Type>,
+	pub attributes: Option<Vec<ParameterAttribute>>,
 	pub default_value: Option<ConstExpr>,
-	pub description:   Option<String>,
+	pub description: Option<String>,
+}
+
+pub enum Parmeter {
+	Literal(LiteralParameter),
+	Generic(TypeParam),
 }
 
 #[derive(Debug, Clone, PartialEq)]
