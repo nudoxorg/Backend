87aeecd6be1c66998684538c69211c875c64373f
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-28 14:37:41 -0400

    fmt

--- ir/ty.rs
+++ ir/ty.rs
@@ -2,7 +2,13 @@
 use serde::{Deserialize, Serialize};
 
 use super::function;
-use crate::{generics::{GenericArg, TraitRef, TypeParam}, parameter::Parameter, primitives::Primitive, protocols::GenericBound, record::SumVariant};
+use crate::{
+	generics::{GenericArg, TraitRef, TypeParam},
+	parameter::Parameter,
+	primitives::Primitive,
+	protocols::GenericBound,
+	record::SumVariant,
+};
 
 /// Universal representation of types across languages.
 #[derive(Debug, Clone, PartialEq)]
@@ -31,22 +37,22 @@ pub enum Type {
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Path {
-	pub path:         String,
+	pub path: String,
 	pub generic_args: Option<Vec<GenericArg>>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct QualifiedPath {
-	pub name:              String,
+	pub name: String,
 	pub generic_arguments: Option<Vec<GenericArg>>,
-	pub self_type:         Box<Type>,
-	pub tr:                Option<Path>,
+	pub self_type: Box<Type>,
+	pub tr: Option<Path>,
 }
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct DynTrait {
-	pub traits:   Vec<PolyTrait>,
+	pub traits: Vec<PolyTrait>,
 	pub lifetime: Option<String>,
 }
 
@@ -55,10 +61,10 @@ pub struct DynTrait {
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct FunctionPointer {
-	pub inputs:         Option<Vec<Parameter>>,
-	pub outputs:        Option<Vec<Parameter>>,
+	pub inputs: Option<Vec<Parameter>>,
+	pub outputs: Option<Vec<Parameter>>,
 	pub generic_params: Option<Vec<TypeParam>>,
-	pub attributes:     Option<Vec<function::Attribute>>,
+	pub attributes: Option<Vec<function::Attribute>>,
 }
 
 // MARK: - PolyTrait
@@ -66,6 +72,6 @@ pub struct FunctionPointer {
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct PolyTrait {
-	pub tr:        TraitRef,
+	pub tr: TraitRef,
 	pub lifetimes: Vec<String>,
 }
