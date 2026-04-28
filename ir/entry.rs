18f28c1a1f4bc8113f0cf449b9dc783605feb792
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-28 17:41:36 -0400

    Introduced nudoxpath type

--- ir/entry.rs
+++ ir/entry.rs
@@ -1,5 +1,8 @@
 ///! We store our data as a nested tree structure to ensure maximum composability for building the structure, and the ease of Serde and so on and so forth. We're able to store references to other objects using absolute paths, and during upload time, a graph is composed once. A (highly) unsafe structure should be considered in the future to avoid this reconstruction between backends.
-use std::collections::{HashMap, HashSet};
+use std::{
+	collections::{HashMap, HashSet},
+	path::Path,
+};
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
@@ -11,10 +14,9 @@ use crate::kind::{Kind, Visibility};
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Entry {
 	// Required
-	pub name: String, // Semantic name for the entry (std::time, or to_string)
-	pub id: i64,      // A real nice way to link together other entries (i64 for more range)
-	pub path: Vec<String>, /* The absolute path leading to the first instance of this entry
-	                   * (primary) */
+	pub name: String,    // Semantic name for the entry (std::time, or to_string)
+	pub path: NudoxPath, // The absolute path leading to the first/canonical instance of this entry
+
 	// aliases = re-exports/other instances of path occurences of same id
 	pub aliases: Option<HashSet<Vec<String>>>,
 	pub kind: Kind,                     // The kind of entry this is
@@ -23,6 +25,15 @@ pub struct Entry {
 	pub documentation: Option<String>, // The associated documentation
 
 	pub members: Option<Vec<EntryRef>>, // References instead of duplicates
+}
+
+/// Stores the path to an exact location within the Nudox registry
+pub enum NudoxPath {
+	/// Is this path from an external dependency?
+	External { path: Path, dependency: String },
+
+	/// Is this path local to the project/crate/whatever
+	Local(Path),
 }
 
 /// Top-level table for the IR entries/roots
