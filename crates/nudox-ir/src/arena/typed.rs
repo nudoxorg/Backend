52c72f1472b11eb7beb636f9872662d254c3ee82
Adrian Valcarcel-Schott <avalsch@proton.me>
2026-07-03 11:48:15 -0500

    minor changes to TypedEntry & Kind APIs

--- crates/nudox-ir/src/arena/typed.rs
+++ crates/nudox-ir/src/arena/typed.rs
@@ -9,7 +9,21 @@ pub struct TypedEntry<T> {
 	_p:    PhantomData<T>,
 }
 
+impl<T> TypedEntry<T> {
+	/// the raw (untyped) inner entry
+	pub fn entry(&self) -> &Entry { &self.inner }
+
+	/// the entry's symbol
+	pub fn sym(&self) -> &Symbol { &self.inner.sym }
+
+	/// the entry's raw kind enum
+	pub fn kind(&self) -> &Kind { &self.inner.kind }
+}
+
 impl<T: EntryKind> TypedEntry<T> {
+	/// # Safety
+	///
+	/// the caller must guarentee that the variant of Kind is type `T`.
 	pub(super) unsafe fn new(entry: &Entry) -> &Self {
 		debug_assert!(kind_as_dyn_any(&entry.kind).is::<T>(), "creating invalid TypedEntry");
 
@@ -18,11 +32,16 @@ impl<T: EntryKind> TypedEntry<T> {
 		unsafe { &*std::ptr::from_ref(entry).cast() }
 	}
 
-	pub fn sym(&self) -> &Symbol { &self.inner.sym }
-
-	pub fn kind(&self) -> &T {
+	// TODO: do we want to impl Deref and make this more like a smart pointer?
+	pub fn get(&self) -> &T {
 		kind_as_dyn_any(&self.inner.kind).downcast_ref().expect("using TypedEntry with incorrect type")
 	}
 }
 
-fn kind_as_dyn_any(kind: &Kind) -> &dyn Any { kind.as_dyn() }
+// helper function to get a &dyn Any from the inner variant of a Kind as &dyn
+// Any, needed because variant_as_dyn returns &dyn EntryKind which doesn't
+// implicitly downcast to &dyn Any for some reason?
+//
+// TODO: ensure that this works as expected through testing, since it could be
+// doing <&dyn EntryKind as &dyn Any> instead of trait upcasting
+fn kind_as_dyn_any(kind: &Kind) -> &dyn Any { kind.variant_as_dyn() }
--- crates/nudox-ir/src/kind.rs
+++ crates/nudox-ir/src/kind.rs
@@ -28,7 +28,7 @@ macro_rules! register_kinds {
 		}
 
 		impl Kind {
-			pub(crate) fn as_dyn(&self) -> &dyn EntryKind {
+			pub(crate) fn variant_as_dyn(&self) -> &dyn EntryKind {
 				match self {
 					$(
 					Kind::$kind(it) => it,
