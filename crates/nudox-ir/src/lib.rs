1b85591abcfab931cb2b30194abc045868c4da47
Adrian Valcarcel-Schott <avalsch@proton.me>
2026-07-03 11:37:57 -0500

    remove existing modules from comments

--- crates/nudox-ir/src/lib.rs
+++ crates/nudox-ir/src/lib.rs
@@ -10,10 +10,6 @@ pub mod symbol;
 pub mod ty;
 
 // pub mod generics;
-// pub mod kind;
 // pub mod parameter;
-// pub mod primitives;
 // pub mod protocols;
-// pub mod record;
 // pub mod syntax;
-// pub mod ty;
