e231300bac1c687f3073e6e8d3e52b92830c3c58
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-07-12 05:11:36 -0400

    Perf: identity - typed URIs, entry

--- source/identity/src/lib.rs
+++ source/identity/src/lib.rs
@@ -4,5 +4,5 @@ pub mod kind_uri;
 pub mod path;
 
 pub use entry_uri::EntryUri;
-pub use global_id::compute as compute_symbol_id;
-pub use kind_uri::KindUri;
+pub use global_id::{compute as compute_symbol_id, TerminusInstance};
+pub use kind_uri::{KindPrefix, KindUri};
