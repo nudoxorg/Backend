c6569dc31e048ba641d4123b14cf3be34aab2baa
Adrian Valcarcel-Schott <avalsch@proton.me>
2026-07-23 02:21:15 -0400

    re-add bon builder API

--- crates/nudox-ir/src/kinds/record.rs
+++ crates/nudox-ir/src/kinds/record.rs
@@ -5,33 +5,13 @@ pub struct Record {
     pub fields: List<EntryIndex<Field>>,
 }
 
-// impl Visitor for Record {
-//     fn visit(&self, f: impl Fn(UntypedEntryIndex)) {
-//         self.fields.visit(f)
-//     }
-
-//     fn visit_mut(&mut self, f: impl Fn(&mut UntypedEntryIndex)) {
-//         self.fields.visit_mut(f)
-//     }
-// }
-
-// #[bon::bon]
-// impl Record {
-//     #[builder(finish_fn(name = finish, vis = ""))]
-//     pub fn new(#[builder(with = FromIterator::from_iter)] fields:
-// List<EntryIdx<Field>>) -> Self {         Record { fields }
-//     }
-// }
-
-// impl<S: record_builder::IsComplete> RecordBuilder<S> {
-//     pub fn build(self, b: &mut EntryBuilder<impl RegistryResolver>) -> Record
-// {         let record = self.finish();
-
-//         b.link_many(record.fields.iter().copied());
-
-//         record
-//     }
-// }
+#[bon::bon]
+impl Record {
+    #[builder]
+    pub fn new(#[builder(with = FromIterator::from_iter)] fields: List<EntryIndex<Field>>) -> Self {
+        Record { fields }
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Visitor, serde::Serialize, serde::Deserialize)]
 pub struct Field {
