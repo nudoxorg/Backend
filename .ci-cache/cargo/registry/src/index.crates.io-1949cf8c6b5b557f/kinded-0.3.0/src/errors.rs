96b70957a00b694cbe6be868baea3cbde7c6a841
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-08-28 12:08:29 -0700

    chore(ci-cache): cache Cargo registry packages k

---
+++ .ci-cache/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/kinded-0.3.0/src/errors.rs
@@ -0,0 +1,44 @@
+/// An error which is returned when parsing of a kind type failures.
+pub struct ParseKindError {
+    kind_type_name: String,
+    given_string: String,
+}
+
+impl ParseKindError {
+    /// This method is used by `kinded` macro to construct an error for FromStr trait and is not
+    /// recommend for a direct usage by users.
+    pub fn from_type_and_string<KindType>(given_string: String) -> ParseKindError {
+        let full_kind_type_name = std::any::type_name::<KindType>();
+        let kind_type_name = full_kind_type_name
+            .split("::")
+            .last()
+            .expect("Type name cannot be empty")
+            .to_string();
+        ParseKindError {
+            kind_type_name,
+            given_string,
+        }
+    }
+}
+
+impl ::core::fmt::Display for ParseKindError {
+    fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+        let Self {
+            kind_type_name,
+            given_string,
+        } = self;
+        write!(f, r#"Failed to parse "{given_string}" as {kind_type_name}"#)
+    }
+}
+
+impl ::core::fmt::Debug for ParseKindError {
+    fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> Result<(), ::core::fmt::Error> {
+        write!(f, "ParseKindError: {self}")
+    }
+}
+
+impl ::std::error::Error for ParseKindError {
+    fn source(&self) -> Option<&(dyn ::std::error::Error + 'static)> {
+        None
+    }
+}
---
+++ .ci-cache/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/kinded_macros-0.3.0/src/lib.rs
@@ -0,0 +1,29 @@
+//! # Kinded
+//!
+//! Generate Rust enum kind types without boilerplate.
+//!
+//! Author: [Serhii Potapov](https://www.greyblake.com/)
+//!
+//! This is a supporting macro crate, that should not be used directly.
+//! For the documentation please refer to [kinded](https://docs.rs/kinded/) crate.
+
+pub(crate) mod gen;
+pub(crate) mod models;
+pub(crate) mod parse;
+
+use proc_macro2::TokenStream;
+use syn::DeriveInput;
+
+#[proc_macro_derive(Kinded, attributes(kinded))]
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    expand_derive(input)
+        .unwrap_or_else(|e| syn::Error::to_compile_error(&e))
+        .into()
+}
+
+fn expand_derive(input: proc_macro::TokenStream) -> Result<TokenStream, syn::Error> {
+    let derive_input: DeriveInput =
+        syn::parse(input).expect("kinded failed parse token stream as DeriveInput");
+    let meta = parse::parse_derive_input(derive_input)?;
+    Ok(gen::generate(meta))
+}
